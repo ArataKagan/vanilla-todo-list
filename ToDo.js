0b909f2a050fb862a7a408e9b971ed3e5debf6f3
Arata Kagan <aratakagan@gmail.com>
2019-07-16 09:09:07 -0700

    remove item functionality added

--- ToDo.js
+++ ToDo.js
@@ -1,30 +1,37 @@
 document.getElementById('add').addEventListener('click', function(){
     let value = document.getElementById('new-task').value;
     if(value) addItemTodo(value);
+    document.getElementById('new-task').value = "";
 })
+
+function removeItem(){
+   let item = this.parentNode;
+   let list = item.parentNode;
+   list.removeChild(item);
+}
 
 function addItemTodo(text) {
     let list = document.getElementById('task-list');
 
     let item = document.createElement('li');
-    item.innerText = text;
-
     let checkBox = document.createElement('input');
     checkBox.type = "checkbox";
-
+    let label = document.createElement('label');
+    label.innerText = text;
     let remove = document.createElement('button');
     remove.classList.add('remove');
     remove.innerText = "Delete";
 
+    remove.addEventListener('click', removeItem);
+
     item.appendChild(checkBox);
+    item.appendChild(label);
     item.appendChild(remove);
 
-    list.appendChild(item);
+    list.insertBefore(item, list.childNodes[0]);
 }
 
-// document.getElementById('add').addEventListener('click', function(){
-//     let value = document.getElementById('item').value;
-//     if (value){
-       
-//     }
-// })
+for(let i=0; document.getElementsByClassName("delete").length > i; i++){
+    document.getElementsByClassName("delete")[i].addEventListener('click', removeItem);
+}
+
